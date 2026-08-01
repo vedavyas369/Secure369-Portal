77c77f251ecb86594b9aad9579669f0ee164ccd9
vedavyas369 <vedavyas.pathivada@secure369.com>
2026-08-01 13:59:45 +0530

    Add Secure369 login page and custom UI

--- app/app.py
+++ app/app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder="../static",
+    template_folder="templates"
+)
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
